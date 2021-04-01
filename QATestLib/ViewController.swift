407d91c18cb531773af37903dbfa9e6d715e91de
Boss Office <dfeetnsb@gmail.com>
2021-04-01 16:35:09 +0700

    add button

--- QATestLib/ViewController.swift
+++ QATestLib/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchDownButton(_ sender: Any) {
+        // insert function here
+    }
+    
 }
 
